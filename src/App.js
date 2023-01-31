117c412b55ce8b7f8cabde3e7ac5e0dd01db1dd8
William Shaner <williamshaner3@gmail.com>
2023-01-31 17:36:16 -0600

    added changing background

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import './index.css';
 import Header from './components/Header/Header';
 import { useState, useEffect } from 'react';
 import Weather from './components/Weather/Weather';
-import Background from './assets/background.jpg';
+// import Background from './assets/background.jpg';
 import Loading from './components/Loading/Loading';
 import Footer from './components/Footer/Footer';
+
 function App() {
   const [error, setError] = useState(false)
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
+  const [background, setBackground] = useState(null)
+  
   const style = {
-    backgroundImage: `url(${Background})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'lightblue',
+    background: `${background}`,
   };
   useEffect(() => {
     const fetchData = async () => {
@@ -24,7 +25,7 @@ function App() {
       });
 
       await fetch(
-        `${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=7abcb94924cccac024edc3185f2247cf`
       )
         .then((res) => res.json())
         .then((result) => {
@@ -42,7 +43,7 @@ function App() {
     <div className="main-container" style={style}>
       <Header />
       {typeof data.main != 'undefined' ? (
-        <Weather weatherData={data} error={error} setError={setError} />
+        <Weather weatherData={data} error={error} setError={setError} setBackground={setBackground} />
       ) : (
         <Loading />
       )}
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Weather.css';
 import { BiRefresh } from 'react-icons/bi';
 import moment from 'moment';
 import Error from '../Error/Error';
 
-const Weather = ({weatherData, error}) => {
+const Weather = ({weatherData, error, setBackground}) => {
+  // var currentTime = moment()  
+  const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')
+  const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')
+  
+  const currentHours = Number(moment().format("HH"))
+  const sunriseHours = Number(new Date(weatherData.sys.sunrise * 1000).getHours())
+  const sunsetHours = Number(new Date(weatherData.sys.sunset * 1000).getHours())
+
+  console.log(currentHours, sunriseHours, sunsetHours)
+
+  useEffect(() => {
+    if (currentHours < sunsetHours && currentHours > sunriseHours) {
+      setBackground('linear-gradient(137deg, rgba(1,128,185,1) 0%, rgba(255,255,255,1) 100%)')
+    }
+    if(currentHours === sunriseHours) {
+      setBackground('linear-gradient(123deg, rgba(255,255,255,1) 0%, rgba(94,195,210,1) 100%)')
+    }
+    if(currentHours === sunsetHours) {
+      setBackground('linear-gradient(137deg, rgba(191,120,238,1) 0%, rgba(247,78,78,0.9985974754558204) 50%, rgba(245,183,95,1) 100%)')
+    }
+    if(currentHours < sunriseHours) {
+      setBackground('linear-gradient(123deg, rgba(0,7,61,1) 0%, rgba(61,32,226,1) 100%)')
+    }
+    else if(currentHours > sunsetHours && currentHours < 24){
+      setBackground('linear-gradient(123deg, rgba(0,7,61,1) 0%, rgba(61,32,226,1) 100%)')
+    }
+  })
+
   return (
+
     <>
     {error ? <Error /> : <main className="weather-container">
       <div className="loading-header">
@@ -13,6 +42,7 @@ const Weather = ({weatherData, error}) => {
           <h4>
             {moment().format('dddd')}, <span>{moment().format('LL')}</span>
           </h4>
+          <h4>{moment().format("hh:mm a")}</h4>
         </div>
         <button className='reload-btn'>
           <BiRefresh
@@ -41,11 +71,11 @@ const Weather = ({weatherData, error}) => {
         <div className="d-flex justify-content-between">
           <p>
             Sunrise:{' '}
-            {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}
+            {sunrise}
           </p>
           <p>
             Sunset:{' '}
-            {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}
+            {sunset}
           </p>
         </div>
       </div>
